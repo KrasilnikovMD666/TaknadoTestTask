6e0c0ec49f0656c29780af37e7f782389079e611
Matvey Krasilnikov <62702684+KrasilnikovMD666@users.noreply.github.com>
2023-09-05 14:19:45 +0300

    Update connect.php

--- app/connect.php
+++ app/connect.php
@@ -1,6 +1,6 @@
 <?php
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-$connect = mysqli_connect("127.0.0.1", "root", "", "company_data");
+$connect = mysqli_connect("127.0.0.1:8080", "root", "root", "company_data");
 
 if($connect === false)
 {
